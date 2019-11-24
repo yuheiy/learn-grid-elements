afa193a8aa27d792463f9fefe682527d568a2e42
Yuhei Yasuda <yuhei.yasuda1003@gmail.com>
2019-11-25 02:42:43 +0900

    init

---
+++ docs/precache-manifest.e82af21cfbf0c3599fd34a41c3db32de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a513353dc0b65ea79f95e13356657c8e",
+    "url": "/learn-grid-elements/index.html"
+  },
+  {
+    "revision": "20717fbb2420442ba33b",
+    "url": "/learn-grid-elements/static/css/main.073b9d51.chunk.css"
+  },
+  {
+    "revision": "246e3525830b6a7a9371",
+    "url": "/learn-grid-elements/static/js/2.65aab8c7.chunk.js"
+  },
+  {
+    "revision": "20717fbb2420442ba33b",
+    "url": "/learn-grid-elements/static/js/main.604b5807.chunk.js"
+  },
+  {
+    "revision": "3051d65eb31705a6d479",
+    "url": "/learn-grid-elements/static/js/runtime-main.7c4db701.js"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,25 +1,298 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const useScrollbarWidth = () => {
+  const [scrollbarWidth, setScrollbarWidth] = React.useState(0);
+
+  React.useEffect(() => {
+    const tester = document.createElement("div");
+    tester.style.width = "calc(100vw - 100%)";
+    document.body.append(tester);
+
+    const onResize = () => {
+      setScrollbarWidth(parseInt(getComputedStyle(tester).width));
+    };
+    window.addEventListener("resize", onResize);
+    onResize();
+  }, []);
+
+  return scrollbarWidth;
+};
 
 function App() {
+  const [containerMaxWidth, setContainerMaxWidth] = React.useState(1200);
+  const [containerMargin, setContainerMargin] = React.useState(16);
+  const [gridColumns, setGridColumns] = React.useState(12);
+  const [gridGutter, setGridGutter] = React.useState(16);
+  const scrollbarWidth = useScrollbarWidth();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <style>{`
+  :root {
+    --container-max-width: ${containerMaxWidth};
+    --container-margin: ${containerMargin};
+    --grid-columns:${gridColumns};
+    --grid-gutter:${gridGutter};
+  }
+
+  @media (min-width: ${containerMaxWidth +
+    containerMargin * 2 +
+    scrollbarWidth}px) {
+    html {
+      font-size: calc(1em + (100vw - ${containerMaxWidth +
+        containerMargin * 2}px) * 0.005);
+    }
+  }
+`}</style>
+
+      <div className="stack -lg">
+        <div className="container">
+          <h1>Gridの構成要素</h1>
+        </div>
+
+        <div className="stack -md">
+          <div className="container">
+            <h2>Container</h2>
+          </div>
+
+          <div className="guide-container">
+            <div className="guide-container-spacer"></div>
+            <div className="guide-container-gap"></div>
+            <div className="guide-container-content"></div>
+            <div className="guide-container-gap"></div>
+            <div className="guide-container-spacer"></div>
+          </div>
+
+          <div className="container">
+            <div className="variables">
+              <div className="variables-key">
+                <code>--container-max-width:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={containerMaxWidth}
+                  min={0}
+                  max={9999}
+                  onChange={event =>
+                    setContainerMaxWidth(Number(event.target.value))
+                  }
+                />{" "}
+                px
+              </div>
+              <div className="variables-key">
+                <code>--container-margin:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={containerMargin}
+                  min={0}
+                  max={9999}
+                  onChange={event =>
+                    setContainerMargin(Number(event.target.value))
+                  }
+                />{" "}
+                px
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <pre>
+              <code>{`<div class="container">
+  …
+</div>`}</code>
+            </pre>
+          </div>
+        </div>
+
+        <div className="stack -md">
+          <div className="container">
+            <h2>Columns</h2>
+          </div>
+
+          <div
+            className="guide-grid"
+            style={{
+              gridTemplateColumns: Array.from(
+                { length: gridColumns },
+                (_, index) => index
+              )
+                .map(
+                  index =>
+                    `${index ? "calc(var(--grid-gutter) / 16 * 1rem) " : ""}1fr`
+                )
+                .join(" ")
+            }}
+          >
+            {Array.from({ length: gridColumns }, (_, index) => index).map(
+              index => (
+                <React.Fragment key={index}>
+                  {index ? <div className="guide-grid-gutter" /> : null}
+                  <div className="guide-grid-column" />
+                </React.Fragment>
+              )
+            )}
+          </div>
+
+          <div className="container">
+            <div className="variables">
+              <div className="variables-key">
+                <code>--grid-columns:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={gridColumns}
+                  min={0}
+                  max={9999}
+                  onChange={event => setGridColumns(Number(event.target.value))}
+                />
+              </div>
+              <div className="variables-key">
+                <code>--grid-gutter:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={gridGutter}
+                  min={0}
+                  max={9999}
+                  onChange={event => setGridGutter(Number(event.target.value))}
+                />{" "}
+                px
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <pre>
+              <code>{`<div class="row">
+  <div class="col -s3">…</div>
+  <div class="col -s3">…</div>
+  <div class="col -s3">…</div>
+</div>`}</code>
+            </pre>
+          </div>
+        </div>
+
+        <div className="stack -md">
+          <div className="container">
+            <h2>Composed</h2>
+          </div>
+
+          <div className="guide-container">
+            <div className="guide-container-spacer"></div>
+            <div className="guide-container-gap"></div>
+            <div
+              className="guide-container-content"
+              style={{ backgroundColor: "transparent" }}
+            >
+              <div
+                className="guide-grid"
+                style={{
+                  gridTemplateColumns: Array.from(
+                    { length: gridColumns },
+                    (_, index) => index
+                  )
+                    .map(
+                      index =>
+                        `${
+                          index ? "calc(var(--grid-gutter) / 16 * 1rem) " : ""
+                        }1fr`
+                    )
+                    .join(" ")
+                }}
+              >
+                {Array.from({ length: gridColumns }, (_, index) => index).map(
+                  index => (
+                    <React.Fragment key={index}>
+                      {index ? <div className="guide-grid-gutter" /> : null}
+                      <div className="guide-grid-column" />
+                    </React.Fragment>
+                  )
+                )}
+              </div>
+            </div>
+            <div className="guide-container-gap"></div>
+            <div className="guide-container-spacer"></div>
+          </div>
+
+          <div className="container">
+            <div className="variables">
+              <div className="variables-key">
+                <code>--container-max-width:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={containerMaxWidth}
+                  min={0}
+                  max={9999}
+                  onChange={event =>
+                    setContainerMaxWidth(Number(event.target.value))
+                  }
+                />{" "}
+                px
+              </div>
+              <div className="variables-key">
+                <code>--container-margin:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={containerMargin}
+                  min={0}
+                  max={9999}
+                  onChange={event =>
+                    setContainerMargin(Number(event.target.value))
+                  }
+                />{" "}
+                px
+              </div>
+              <div className="variables-key">
+                <code>--grid-columns:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={gridColumns}
+                  min={0}
+                  max={9999}
+                  onChange={event => setGridColumns(Number(event.target.value))}
+                />
+              </div>
+              <div className="variables-key">
+                <code>--grid-gutter:</code>
+              </div>
+              <div className="variables-value">
+                <input
+                  type="number"
+                  value={gridGutter}
+                  min={0}
+                  max={9999}
+                  onChange={event => setGridGutter(Number(event.target.value))}
+                />{" "}
+                px
+              </div>
+            </div>
+          </div>
+
+          <div className="container">
+            <pre>
+              <code>{`<div class="container">
+  <div class="row">
+    <div class="col -s3">…</div>
+    <div class="col -s3">…</div>
+    <div class="col -s3">…</div>
+  </div>
+</div>`}</code>
+            </pre>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
 
